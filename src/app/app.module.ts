b1e5a7c4f4cffb9a94c752d027b372a8969b0493
Eric Zayas <ezayas16@icloud.com>
2017-09-05 23:21:21 -0400

    added home component set up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { AnswerComponent } from './components/answer/answer.component';
+import { HomeComponent } from './components/home/home.component';
+
+const appRoutes: Routes = [
+ {path: '' , component: HomeComponent},
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     KeyboardComponent,
-    AnswerComponent
+    AnswerComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
