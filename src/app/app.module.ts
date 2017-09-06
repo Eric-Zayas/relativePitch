ec1489cb0c6c2bfca736141b3455437d189b3af4
Eric Zayas <ezayas16@icloud.com>
2017-09-05 23:05:28 -0400

    installed dependencies and added basic components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { KeyboardComponent } from './components/keyboard/keyboard.component';
+import { AnswerComponent } from './components/answer/answer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    KeyboardComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule
